d92d1f4ab140d16a5e58440bed06cfdab4c8aa2f
Txs285 <tatiana.stepourska@cra-arc.gc.ca>
2017-01-20 16:12:06 -0500

    update

--- CTS_dataprep/implementation/src/ca/gc/cra/fxit/ctsagent/task/xml/IHelper.java
+++
@@ -1,7 +0,0 @@
-package ca.gc.cra.fxit.ctsagent.task.xml;
-
-import ca.gc.cra.fxit.ctsagent.model.PackageInfo;
-
-public interface IHelper {
-	int invoke(PackageInfo p);
-}
--- CTS_dataprep/implementation/web/layout/dateModified-e.jsp
+++
@@ -1,5 +0,0 @@
-<dl id="wb-dtmd" property="dateModified">
-<dt>Date modified:&#32;</dt>
-<dd><time>2014-05-30</time></dd>
-</dl>
-</main>
