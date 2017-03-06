98d959bfa90bf307e9957eb25ce43c0ccd81057f
Txs285 <tatiana.stepourska@cra-arc.gc.ca>
2017-03-06 09:50:56 -0500

    test commit after add to index

---
+++ pancorp/src/com/pancorp/tbroker/main/Main.java
@@ -0,0 +1,11 @@
+package com.pancorp.tbroker.main;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+	private static Logger lg = Logger.getLogger(Main.class);
+	
+	public static void main(String[] args){
+		lg.info("Hello world");
+	}
+}
